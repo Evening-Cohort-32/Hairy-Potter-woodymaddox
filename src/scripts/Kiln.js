5c4f0696b8f3367138dfcdc7c0ad89fbbb479d0e
WOODY MADDOX <WOODY.E.MADDOX@GMAIL.COM>
2026-03-11 13:07:29 -0500

    Kiln module update

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,11 @@
+export const firePottery = (potteryItem, temp) => {
+  potteryItem.fired = true;
+
+  if (temp > 2200) {
+    potteryItem.cracked = true;
+  } else if (temp <= 2200) {
+    potteryItem.cracked = false;
+  }
+
+  return potteryItem;
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+import { firePottery } from "./Kiln.js";
 import { makePottery } from "./PotteryWheel.js";
 
 const mug = makePottery("Mug", 2, 10);
@@ -6,4 +7,16 @@ const teapot = makePottery("Teapot", 5, 11);
 const vase = makePottery("Vase", 5, 8);
 const planter = makePottery("Planter", 5, 11);
 
-console.log(potteryItem);
+const firedMug = firePottery(mug, 2000);
+const firedPlate = firePottery(plate, 2300);
+const firedTeapot = firePottery(teapot, 2600);
+const firedVase = firePottery(vase, 3000);
+const firedPlanter = firePottery(planter, 1500);
+
+console.log(mug);
+
+console.log(firedMug);
+console.log(firedPlate);
+console.log(firedTeapot);
+console.log(firedVase);
+console.log(firedPlanter);
